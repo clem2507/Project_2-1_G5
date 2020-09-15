9a0f7839096f19a9f5be990765a401e6f32dbaf6
Aaron Schapira <aaronschapira@gmail.com>
2020-09-15 10:50:57 +0200

    Update Of board

--- Abalon/src/main/java/Abalon/UI/Board.java
+++ Abalon/src/main/java/Abalon/UI/Board.java
@@ -1,7 +1,11 @@
 package Abalon.UI;
 
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Box;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
@@ -11,13 +15,34 @@ public class Board {
 
     private int[] values;
     public static ArrayList<Circle> circles;
-    public static ArrayList<Text> texts;
+    public Polygon hexagon;
+    public Text player1;
+    public Text player2;
 
     private final double RADIUS =30;
 
     public Board(){
         values = createValues();
         circles = createCircles();
+        hexagon = createHexagon();
+        player1 = new Text("test");
+    }
+
+    public static Polygon createHexagon(){
+        Polygon hexagon = new Polygon();
+
+        //Adding coordinates to the hexagon
+        hexagon.getPoints().addAll(new Double[]{
+                300.0, 12.0, //1
+                700.0, 12.0, //1
+                850.0, 350.0, //2
+                700.0, 688.0, //3
+                300.0, 688.0, //3
+                150.0, 350.0, //2
+        });
+        hexagon.setFill(Color.ORANGE);
+
+        return hexagon;
     }
 
  /*
@@ -28,7 +53,7 @@ public class Board {
 
         // 9 loops to create all circles (define position)
         // 1 loop per line of circles in the board
-        double x_coord = 335;
+        double x_coord = 485;
         double y_coord = 62;
         int index=5;
 
@@ -42,7 +67,7 @@ public class Board {
             x_coord += RADIUS*2 + 15;
         }
         y_coord += RADIUS*2+10;
-        x_coord = 305;
+        x_coord = 455;
 
         for(int i=0; i<6;i++){
             Circle circle = new Circle(RADIUS);
@@ -54,7 +79,7 @@ public class Board {
             x_coord += RADIUS*2 + 15;
         }
         y_coord += RADIUS*2+10;
-        x_coord = 275;
+        x_coord = 415;
 
         for(int i=0; i<7;i++){
             Circle circle = new Circle(RADIUS);
@@ -67,7 +92,7 @@ public class Board {
 
         }
         y_coord += RADIUS*2+10;
-        x_coord = 245;
+        x_coord = 380;
 
 
         for(int i=0; i<8;i++){
@@ -81,7 +106,7 @@ public class Board {
             index++;
         }
         y_coord += RADIUS*2+10;
-        x_coord = 205;
+        x_coord = 340;
 
         for(int i=0; i<9;i++){
             Circle circle = new Circle(RADIUS);
@@ -93,7 +118,7 @@ public class Board {
             x_coord += RADIUS*2 + 15;
         }
         y_coord += RADIUS*2+15;
-        x_coord = 245;
+        x_coord = 380;
         for(int i=0; i<8;i++){
             Circle circle = new Circle(RADIUS);
             circle.setFill(Color.BISQUE);
@@ -104,7 +129,7 @@ public class Board {
             x_coord += RADIUS*2 + 15;
         }
         y_coord += RADIUS*2+15;
-        x_coord = 275;
+        x_coord = 415;
 
         for(int i=0; i<7;i++){
             Circle circle = new Circle(RADIUS);
@@ -116,7 +141,7 @@ public class Board {
             x_coord += RADIUS*2 + 15;
         }
         y_coord += RADIUS*2+10;
-        x_coord = 305;
+        x_coord = 455;
 
         for(int i=0; i<6;i++){
             Circle circle = new Circle(RADIUS);
@@ -128,7 +153,7 @@ public class Board {
             x_coord += RADIUS*2 + 15;
         }
         y_coord += RADIUS*2+15;
-        x_coord = 335;
+        x_coord = 485;
 
         for(int i=0; i<5;i++){
             Circle circle = new Circle(RADIUS);
@@ -142,12 +167,12 @@ public class Board {
         y_coord += RADIUS*2+10;
 
         //Creation of the circles for the score FOR PLAYER 1
-        Circle c1 = createCircleForScore(25, 1100,150, Color.BISQUE);
-        Circle c2 = createCircleForScore(25, 1075,200, Color.BISQUE);
-        Circle c3 = createCircleForScore(25, 1125,200, Color.BISQUE);
-        Circle c4 = createCircleForScore(25, 1050,250, Color.BISQUE);
-        Circle c5 = createCircleForScore(25, 1100,250, Color.BISQUE);
-        Circle c6 = createCircleForScore(25, 1150,250, Color.BISQUE);
+        Circle c1 = createCircleForScore(25, 150,250, Color.BISQUE);
+        Circle c2 = createCircleForScore(25, 125,300, Color.BISQUE);
+        Circle c3 = createCircleForScore(25, 175,300, Color.BISQUE);
+        Circle c4 = createCircleForScore(25, 100,350, Color.BISQUE);
+        Circle c5 = createCircleForScore(25, 150,350, Color.BISQUE);
+        Circle c6 = createCircleForScore(25, 200,350, Color.BISQUE);
 
         circles.add(c1);
         circles.add(c2);
@@ -157,12 +182,12 @@ public class Board {
         circles.add(c6);
 
         //Creation of the circles for the score FOR PLAYER 2
-        Circle c11 = createCircleForScore(25, 1100,500, Color.BISQUE);
-        Circle c21 = createCircleForScore(25, 1075,550, Color.BISQUE);
-        Circle c31 = createCircleForScore(25, 1125,550, Color.BISQUE);
-        Circle c41 = createCircleForScore(25, 1050,600, Color.BISQUE);
-        Circle c51 = createCircleForScore(25, 1100,600, Color.BISQUE);
-        Circle c61 = createCircleForScore(25, 1150,600, Color.BISQUE);
+        Circle c11 = createCircleForScore(25, 1130,250, Color.BISQUE);
+        Circle c21 = createCircleForScore(25, 1105,300, Color.BISQUE);
+        Circle c31 = createCircleForScore(25, 1155,300, Color.BISQUE);
+        Circle c41 = createCircleForScore(25, 1080,350, Color.BISQUE);
+        Circle c51 = createCircleForScore(25, 1130,350, Color.BISQUE);
+        Circle c61 = createCircleForScore(25, 1180,350, Color.BISQUE);
 
         circles.add(c11);
         circles.add(c21);
@@ -170,13 +195,6 @@ public class Board {
         circles.add(c41);
         circles.add(c51);
         circles.add(c61);
-
-        //Creation of the text in the scene
-        Text player1 = scoreText("Player 1", 1200 ,100, 20);
-        Text player2 = scoreText("Player 2", 1200 ,700, 20);
-
-//        texts.add(player1);
-//        texts.add(player2);
 
         return circles;
     }
@@ -189,14 +207,6 @@ public class Board {
         circle.setCenterY(posY);
 
         return circle;
-    }
-
-    public Text scoreText(String text, double posX, double posY, double size){
-        Text player = new Text(text);
-        player.setX(posX);
-        player.setY(posY);
-        player.setFont(Font.font(size));
-        return player;
     }
 
  /*
--- Abalon/src/main/java/Abalon/UI/Hexagon.java
+++ Abalon/src/main/java/Abalon/UI/Hexagon.java
@@ -1,17 +1,11 @@
 package Abalon.UI;
 
-
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Polygon;
-import javafx.scene.text.Text;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 public class Hexagon extends Application {
 
@@ -21,26 +15,16 @@ public class Hexagon extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        //Creating an object of the class Polygon
-        Polygon hexagon = new Polygon();
-
-        //Adding coordinates to the hexagon
-        hexagon.getPoints().addAll(new Double[]{
-                300.0, 12.0, //1
-                700.0, 12.0, //1
-                850.0, 350.0, //2
-                700.0, 688.0, //3
-                300.0, 688.0, //3
-                150.0, 350.0, //2
-        });
-
-        hexagon.setFill(Color.ORANGE);
-        Group root = new Group(hexagon);
+        //Creating an object of Board, which construct a board
         Board board = new Board();
-        root.getChildren().addAll(Board.circles);
-        //root.getChildren().addAll(Board.texts);
-        Scene scene = new Scene(root ,WIDTH, HEIGHT);
-        primaryStage.setTitle("Sample Application");
+
+        //Creation of a Border pane to make our scene easier to construct
+        BorderPane pane = new BorderPane();
+        pane.setCenter(board.hexagon);
+        pane.getChildren().addAll(Board.circles);
+
+        Scene scene = new Scene(pane ,WIDTH, HEIGHT);
+        primaryStage.setTitle("Abalone");
         primaryStage.setScene(scene);
         primaryStage.show();
 
