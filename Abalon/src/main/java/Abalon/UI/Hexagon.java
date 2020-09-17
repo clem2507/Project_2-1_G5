bc0e313d4abe0dd8ac9b069b0e6df39e8103eefa
Aaron Schapira <aaronschapira@gmail.com>
2020-09-17 11:37:37 +0200

    Text in Board

--- Abalon/src/main/java/Abalon/UI/Hexagon.java
+++ Abalon/src/main/java/Abalon/UI/Hexagon.java
@@ -3,6 +3,11 @@ package Abalon.UI;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -22,6 +27,26 @@ public class Hexagon extends Application {
         BorderPane pane = new BorderPane();
         pane.setCenter(board.hexagon);
         pane.getChildren().addAll(Board.circles);
+        
+        Text player1 = new Text("Player 1");
+        player1.setTranslateX(110);
+        player1.setTranslateY(200);
+        player1.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+        //Setting the color
+        player1.setFill(Color.ORANGE);
+        //Setting the Stroke
+        player1.setStrokeWidth(2);
+        pane.getChildren().add(player1);
+
+        Text player2 = new Text("Player 2");
+        player2.setTranslateX(1090);
+        player2.setTranslateY(200);
+        player2.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+        //Setting the color
+        player2.setFill(Color.ORANGE);
+        //Setting the Stroke
+        player2.setStrokeWidth(2);
+        pane.getChildren().add(player2);
 
         Scene scene = new Scene(pane ,WIDTH, HEIGHT);
         primaryStage.setTitle("Abalone");
