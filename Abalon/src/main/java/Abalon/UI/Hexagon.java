00c36540303800c742ee3ca320312d42926b77ba
Aaron Schapira <aaronschapira@gmail.com>
2020-09-20 22:12:17 +0200

    Update Hexagon.java
    Deleting the main method of hexagon

--- Abalon/src/main/java/Abalon/UI/Hexagon.java
+++ Abalon/src/main/java/Abalon/UI/Hexagon.java
@@ -51,46 +51,6 @@ public class Hexagon extends Application {
         BorderPane pane = new BorderPane();
         pane.setCenter(board.hexagon);
         pane.getChildren().addAll(board.circles);
-        
-        Text player1 = new Text("Player 1");
-        player1.setTranslateX(110);
-        player1.setTranslateY(200);
-        player1.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
-        //Setting the color
-        player1.setFill(Color.ORANGE);
-        //Setting the Stroke
-        player1.setStrokeWidth(2);
-        pane.getChildren().add(player1);
-
-        Text player2 = new Text("Player 2");
-        player2.setTranslateX(1090);
-        player2.setTranslateY(200);
-        player2.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
-        //Setting the color
-        player2.setFill(Color.ORANGE);
-        //Setting the Stroke
-        player2.setStrokeWidth(2);
-        pane.getChildren().add(player2);
-
-        Scene scene = new Scene(pane ,WIDTH, HEIGHT);
-        primaryStage.setTitle("Abalone");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
-
-    public void create(Stage primaryStage){
-        //Creating an object of Board, which construct a board
-        BoardUI board = new BoardUI();
-
-        //Creation of a Border pane to make our scene easier to construct
-        BorderPane pane = new BorderPane();
-        pane.setCenter(board.hexagon);
-        pane.getChildren().addAll(board.circles);
 
         Text player1 = new Text("Player 1");
         player1.setTranslateX(110);
@@ -112,9 +72,11 @@ public class Hexagon extends Application {
         player2.setStrokeWidth(2);
         pane.getChildren().add(player2);
 
-        Scene scene = new Scene(pane ,WIDTH, HEIGHT);
+        Scene scene = new Scene(pane, WIDTH, HEIGHT);
         primaryStage.setTitle("Abalone");
         primaryStage.setScene(scene);
         primaryStage.show();
+
     }
+
 }
