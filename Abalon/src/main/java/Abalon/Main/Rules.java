48816c8573cff4f190a8d314ebb547b4e9fb5d72
MathiasYap <62245724+MathiasYap@users.noreply.github.com>
2020-09-26 21:42:03 +0200

    created a checkOutOfBounds method

--- Abalon/src/main/java/Abalon/Main/Rules.java
+++ Abalon/src/main/java/Abalon/Main/Rules.java
@@ -10,14 +10,99 @@ public class Rules {
 
         }
     }
+    /*
+        checks if a suggested move will leave a marble out of bounds
 
+        @param board A 2D integer array that contains the colors of every position on the hexagon.
+               1 = a marble that belongs to player 1.
+               2 = a marble that belongs to player 2.
+               0 = a position that does not contain a marble.
+               -1 = a position in the array that is out of bounds.
+
+        @param direction an enum that contains the direction of the suggested move.
+        @param marble An integer array with length 2. The first int is the row the marble is at and the second int
+               is the column the marble is at.
+
+       @returns a boolean. True if the move will leave the marble out of bounds, false if it will not.
+     */
     private static boolean checkOutOfBounds(int[] marble, MoveDirection direction, int[][] board) {
-        switch(direction) {
+
+        switch (direction) {
+
             case TOP_LEFT:
-                int row = marble[0] - 1;
-                int column = marble[1] - 1;
-                if (board[row][column] == -1) { return true; }
-                else { return false; }
+                //check if a marble is on the top half.
+                if (marble[0] <= 5) {
+                    //check if the marble is on the far left.
+                    if (marble[1] == 0) {
+                        return true;
+                    }
+                } else {
+                    return false;
+                }
+
+            case LEFT:
+                //check if a marble is on the far left.
+                if (marble[1] == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+
+            case BOTTOM_LEFT:
+                //check if a marble is on the bottom half.
+                if (marble[0] <= 5) {
+                    //check if the marble is on the far left.
+                    if (marble[1] == 0) {
+                        return true;
+                    }
+                } else {
+                    return false;
+                }
+
+            case TOP_RIGHT:
+                //check if a marble is on the top half.
+                if (marble[0] <= 5) {
+                    //check if that marble is at the absolute top
+                    if (marble[0] == 0) {
+                        return true;
+                    }
+                    //check if the marble is on the far right.
+                    int rowAbove = marble[0] - 1;
+                    if (board[rowAbove][marble[1]] == -1) {
+                        return true;
+                    }
+                } else {
+                    return false;
+                }
+        }
+
+        case RIGHT:
+        //check if the marble is on the far right.
+        if (board[marble[0]][marble[1] + 1] == -1) {
+            return true;
+        } else {
+            return false;
+        }
+
+        case BOTTOM_RIGHT:
+        //check if a marble is on the bottom half.
+        if (marble[0] >= 5) {
+            //check if that marble is at the absolute bottom.
+            if (marble[0] == 9) {
+                return true;
+            }
+            //check if the marble is on the far right.
+            int rowBelow = marble[0] + 1;
+            if (board[rowBelow][marble[1]] == -1) {
+                return true;
+            }
+        } else {
+            return false;
+        }
+
+
+
+
 
         }
     }
