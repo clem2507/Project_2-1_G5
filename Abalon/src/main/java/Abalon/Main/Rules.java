aa4fd6f661d1d14491d0f88695051b43a1492720
MathiasYap <62245724+MathiasYap@users.noreply.github.com>
2020-10-01 13:41:46 +0200

    working on checkMove

--- Abalon/src/main/java/Abalon/Main/Rules.java
+++ Abalon/src/main/java/Abalon/Main/Rules.java
@@ -1,13 +1,6 @@
 public class Rules {
     public static boolean checkMove(List<int[][]> pushing, List<int[][]> pushed, int[][] board, MoveDirection direction) {
-        switch(board.length) {
-            case "1":
-                checkOutOfBounds(pushing[0][]);
-
-
-                if (pushing.length == 0 && ) { return true; }
-
-
+        
         }
     }
     /*
