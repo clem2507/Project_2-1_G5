8c65fca4ee845dc1edd8f737f4b57b7c0fd98091
clemdetry <c.detry@student.maastrichtuniversity.nl>
2020-10-02 12:49:53 +0200

    Background + combo box fixed

--- Abalon/src/main/java/Abalon/UI/HomePage.java
+++ Abalon/src/main/java/Abalon/UI/HomePage.java
@@ -7,11 +7,14 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
@@ -19,55 +22,42 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.util.Collections;
+
 
 public class HomePage extends Application {
 
-    private final double WIDTH = 1260;
-    private final double HEIGHT = 700;
+    private final double WIDTH = 1344;
+    private final double HEIGHT = 756;
     Hexagon hexagon;
 
     @Override
     public void start(Stage primaryStage) {
         Group pane = new Group();
 
+        Image background = new Image("Abalon/background.jpg");
+        ImageView view = new ImageView(background);
+        pane.getChildren().addAll(view);
+
         //Creation of the title of the project
         Text title = new Text("ABALONE");
         title.setTranslateX(460);
-        title.setTranslateY(200);
+        title.setTranslateY(180);
         title.setFont(Font.font("ayuthaya", FontWeight.BOLD, FontPosture.REGULAR, 90));
         //Setting the color
-        title.setFill(Color.ORANGE);
+        title.setFill(Color.BLACK);
         //Setting the Stroke
         pane.getChildren().add(title);
 
         //Creation of the text for the group number
         Text groupNumber = new Text("GROUP 5");
         groupNumber.setTranslateX(530);
-        groupNumber.setTranslateY(500);
+        groupNumber.setTranslateY(480);
         groupNumber.setFont(Font.font("ayuthaya", FontWeight.BOLD, FontPosture.REGULAR, 50));
         //Setting the color
-        groupNumber.setFill(Color.ORANGE);
+        groupNumber.setFill(Color.BLACK);
         //Setting the Stroke
         pane.getChildren().add(groupNumber);
-
-        //Creation of a button to access the game
-        Button play = new Button();
-        play.setText("Start");
-        play.setTranslateX(570);
-        play.setTranslateY(350);
-        play.setTextFill(Color.ORANGE);
-        play.setFont(Font.font("ayuthaya", FontWeight.BOLD, FontPosture.REGULAR, 30));
-        pane.getChildren().add(play);
-
-        //Button listener
-        play.setOnAction(new EventHandler() {
-            @Override
-            public void handle(Event event) {
-                hexagon = new Hexagon();
-                System.out.println("Start Button clicked, game is gonna start!");
-                hexagon.start(primaryStage);
-            }
-        });
 
         //Creation of a combo box to choose the game Mode
         ComboBox gameChoice = new ComboBox();
@@ -77,12 +67,35 @@ public class HomePage extends Application {
                 "AI CHOICE 2",
                 "PRACTICE MODE");
 
+        gameChoice.getSelectionModel().selectFirst();
         gameChoice.setTranslateX(560);
-        gameChoice.setTranslateY(270);
+        gameChoice.setTranslateY(250);
         gameChoice.setPrefSize(150, 35);
         pane.getChildren().add(gameChoice);
 
+        //Creation of a button to access the game
+        Button play = new Button();
+        play.setText("Start");
+        play.setTranslateX(570);
+        play.setTranslateY(330);
+        play.setTextFill(Color.BLACK);
+        play.setFont(Font.font("ayuthaya", FontWeight.BOLD, FontPosture.REGULAR, 30));
+        pane.getChildren().add(play);
+
+        //Button listener
+        play.setOnAction((EventHandler) event -> {
+            if (gameChoice.getValue().equals("PLAYER VS PLAYER")) {
+                hexagon = new Hexagon();
+                System.out.println("Start Button clicked, game is gonna start!");
+                hexagon.start(primaryStage);
+            }
+            else {
+                System.out.println("Please select PLAYER VS PLAYER game mode!");
+            }
+        });
+
         Scene scene = new Scene(pane ,WIDTH, HEIGHT);
+        primaryStage.setResizable(false);
         primaryStage.setTitle("Abalone");
         primaryStage.setScene(scene);
         primaryStage.show();
