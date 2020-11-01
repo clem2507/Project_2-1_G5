aace7f606111f08cea96258407a44fc9f91432d7
Adeleimpa <56159423+Adeleimpa@users.noreply.github.com>
2020-11-01 21:45:15 +0100

    *

--- Abalon/src/main/java/Abalon/AI/AlphaBeta.java
+++ Abalon/src/main/java/Abalon/AI/AlphaBeta.java
@@ -4,7 +4,7 @@ public class AlphaBeta {
 
     /**
      * bot able to play using Alpha-Beta algorithm:
-     * 1: make game tree (max nr of nodes, but impossible to have everything)
+     * 1: make game tree (max nr of nodes, but impossible to have everything) >>GameTree class
      * 2: prune the tree using alpha-beta pruning (>>get less nodes)
      * 3: evaluate each node (evaluation function)
      * 4: bot applies first move
