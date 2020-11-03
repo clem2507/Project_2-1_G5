5d037439e8c3054dbc32c1b0be8cc59088c2dc6b
Adeleimpa <56159423+Adeleimpa@users.noreply.github.com>
2020-11-03 15:44:21 +0100

    gametree

--- Abalon/src/main/java/Abalon/AI/GameTree.java
+++ Abalon/src/main/java/Abalon/AI/GameTree.java
@@ -1,9 +1,18 @@
 package Abalon.AI;
 
+import java.util.List;
+
 public class GameTree {
     /**
      * Creates the game tree data structure
      * each node represents a state
      * then the algorithms will take care of going through the tree and pick a move
      */
+
+    private List<Edge> edges;
+    private List<Node> nodes;
+
+    public GameTree(int[][]board){
+        //create Node based on the board
+    }
 }
