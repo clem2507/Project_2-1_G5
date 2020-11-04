b84b6ffaf043574bd1f0afd556e1cd224080a8c2
Aaron Schapira <aaronschapira@gmail.com>
2020-11-04 17:55:39 +0100

    Update Node.java
    update of the node class

--- Abalon/src/main/java/Abalon/AI/Node.java
+++ Abalon/src/main/java/Abalon/AI/Node.java
@@ -4,6 +4,20 @@ import java.util.List;
 
 public class Node{
 
+    public static int[][] rootBoardState = new int[][]{
+
+            {1, 1, 1, 1, 1, -1, -1, -1, -1},
+            {1, 1, 1, 1, 1,  1, -1, -1, -1},
+            {0, 0, 1, 1, 1,  0,  0, -1, -1},
+            {0, 0, 0, 0, 0,  0,  0,  0, -1},
+            {0, 0, 0, 0, 0,  0,  0,  0,  0},
+            {0, 0, 0, 0, 0,  0,  0,  0, -1},
+            {0, 0, 2, 2, 2,  0,  0, -1, -1},
+            {2, 2, 2, 2, 2,  2, -1, -1, -1},
+            {2, 2, 2, 2, 2, -1, -1, -1, -1}
+
+    };
+
     private double score; //represent the score of a node, defined by the evaluation function
     private int[][] boardState; //2D array of type cell colors, represent the state of the board
 
@@ -13,13 +27,14 @@ public class Node{
 
     public Node(int[][] boardState, int score) {
         //get the values of the state and its score with their getters or not?
-        this.boardState = getBoardState();
-        this.score = getScore();
+        this.boardState = boardState;
+        this.score = score;
     }
 
-    public double getScore() { //get the score by the evaluation function
-//        EvaluationFunction evaluationFunction = new EvaluationFunction(1, cellColor, rootCellColor);
-//        score = evaluationFunction.evaluate();
+    public double getScore(int currentPlayer, int[][] currentBoardState) { //get the score by the evaluation function
+
+        EvaluationFunction evaluationFunction = new EvaluationFunction(currentPlayer, currentBoardState, rootBoardState);
+        score = evaluationFunction.evaluate();
         return score;
     }
 
