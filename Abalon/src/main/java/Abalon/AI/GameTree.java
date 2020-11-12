75baf85559f716369c2a059d70ab8df6a8f05341
Adeleimpa <56159423+Adeleimpa@users.noreply.github.com>
2020-11-12 15:56:36 +0100

    getPossibleMove into GameTree class

--- Abalon/src/main/java/Abalon/AI/GameTree.java
+++ Abalon/src/main/java/Abalon/AI/GameTree.java
@@ -1,6 +1,7 @@
 package Abalon.AI;
 
 import java.util.*;
+import Abalon.Main.Rules;
 
 public class GameTree {
     /**
@@ -8,6 +9,8 @@ public class GameTree {
      * each node represents a state
      * then the algorithms will take care of going through the tree and pick a move
      */
+    private Rules rules;
+    private ArrayList<int[][]> allPossibleMoves = new ArrayList<>();
 
     private Node root;
 
@@ -18,7 +21,7 @@ public class GameTree {
     private final int generation = 3;
     private int counter = 1;
 
-    private static ArrayList<int[][]> list;
+    //private static ArrayList<int[][]> list;
 
     public GameTree(){
         //create the tree, start with the initial board
@@ -63,7 +66,7 @@ public class GameTree {
 
         // ArrayList<int[][]> childrenStates = findPossibleMoves(currentPlayer, currentBoardState);
 
-        ArrayList<int[][]> childrenStates = findRandomPossibleMoves(60);
+        ArrayList<int[][]> childrenStates = getPossibleMoves(currentBoardState, currentPlayer);
 
         for(int[][] child : childrenStates){
 
@@ -85,8 +88,95 @@ public class GameTree {
         }
    }
 
+    public ArrayList<int[][]> getPossibleMoves(int[][] board, int playerTurn) {
+        //TODO take the board and create move objects for every possible move the player can make
 
-    public static ArrayList<int[][]> findRandomPossibleMoves(int size) {
+
+        allPossibleMoves.addAll(getSingleMarbleMoves(board, playerTurn));
+
+
+
+        return allPossibleMoves; //this will be an arraylist containing move objects
+    }
+
+    public ArrayList<int[][]> getSingleMarbleMoves(int[][] board, int playerTurn) {
+        ArrayList<int[][]> singleMarbleMoves = new ArrayList<>();
+
+        for(int i = 0; i<board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == playerTurn) { //search the entire array for marbles belonging to player 1
+                    Move mv = new Move(); //create a move object
+
+                    mv.pushing = new int[3][2]; //add the first marble to pushing for the move
+                    mv.pushing[0][0] = j;       //QUESTION: in what form does pushing usually come? Is this valid for the rules class?
+                    mv.pushing[0][1] = i;
+
+                    mv.board = board.clone(); //clone the board as it is as a new object that can be altered
+                    mv.turn = playerTurn;
+
+                    mv.dir = MoveDirection.TOP_LEFT;
+                    rules = new Rules(mv);
+                    if (rules.checkMove(mv.pushing, mv.dir, mv.board, mv.turn)) {
+                        rules.move(); //change the cloned board to the state after the move has been made
+                        singleMarbleMoves.add(mv.board); //add this state to the array of possible moves
+                    }
+
+                    mv.dir = MoveDirection.LEFT;
+                    mv.board = board.clone(); //create a new object from the game state before the move
+                    //rules = new Rules(mv);
+                    if (rules.checkMove(mv.pushing, mv.dir, mv.board, mv.turn)) {
+                        rules.move();
+                        singleMarbleMoves.add(mv.board);
+                    }
+
+                    mv.dir = MoveDirection.BOTTOM_LEFT;
+                    mv.board = board.clone();
+                    if (rules.checkMove(mv.pushing, mv.dir, mv.board, mv.turn)) {
+                        rules.move();
+                        singleMarbleMoves.add(mv.board);
+                    }
+
+                    mv.dir = MoveDirection.TOP_RIGHT;
+                    mv.board = board.clone();
+                    if (rules.checkMove(mv.pushing, mv.dir, mv.board, mv.turn)) {
+                        rules.move();
+                        singleMarbleMoves.add(mv.board);
+                    }
+
+                    mv.dir = MoveDirection.RIGHT;
+                    mv.board = board.clone();
+                    if (rules.checkMove(mv.pushing, mv.dir, mv.board, mv.turn)) {
+                        rules.move();
+                        singleMarbleMoves.add(mv.board);
+                    }
+
+                    mv.dir = MoveDirection.BOTTOM_RIGHT;
+                    mv.board = board.clone();
+                    if (rules.checkMove(mv.pushing, mv.dir, mv.board, mv.turn)) {
+                        rules.move();
+                        singleMarbleMoves.add(mv.board);
+                    }
+                }
+
+            }
+        }
+        return singleMarbleMoves;
+    }
+
+    public ArrayList<int[][]> doubleMarblemMoves(int[][] board, int playerTurn) {
+        ArrayList<int[][]> doubleMarbleMoves = new ArrayList<>();
+        return doubleMarbleMoves;
+        //TODO find all combinations of two marbles then use the same operations as singleMarbleMoves
+    }
+
+    public ArrayList<int[][]> tripleMarbleMoves(int[][] board, int playerTurn) {
+        ArrayList<int[][]> tripleMarbleMoves = new ArrayList<>();
+        return tripleMarbleMoves;
+        //TODO find all combinations of three marbles then use the same method operations as singleMarbleMoves
+    }
+
+
+    /*public static ArrayList<int[][]> findRandomPossibleMoves(int size) {
 
         Random r = new Random();
 
@@ -118,7 +208,9 @@ public class GameTree {
             list.add(cellColor);
         }
         return list;
-    }
+    }*/
+
+
 
     public ArrayList<Node> getNodes() {
         return nodes;
