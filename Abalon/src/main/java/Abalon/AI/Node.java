b154b0c28c598f97c4cf3a1bb50862b7fb17c056
clemdetry <c.detry@student.maastrichtuniversity.nl>
2020-11-19 19:30:55 +0100

    nvm

---
+++ Abalon/src/main/java/Abalon/AI/Node.java
@@ -0,0 +1,40 @@
+package Abalon.AI;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node{
+
+    private double score; //represent the score of a node, defined by the evaluation function
+    private int[][] boardState; //2D array of type cell colors, represent the state of the board
+
+    //    private List<Node> children = new ArrayList<>();
+    private Node parent = null;
+    private Node root = null;
+
+    public Node(int[][] boardState, double score) {
+        this.boardState = boardState;
+        this.score = score;
+    }
+
+    public double getScore() { //get the score by the evaluation function
+        return score;
+    }
+
+    public int[][] getBoardState() {
+        return boardState;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+//  ???????
+//    public List<Node> setChildren(){
+//
+//    }
+//
+//
+//    public List<Node> getChildren() {
+//        return children;
+//    }
+
+}
