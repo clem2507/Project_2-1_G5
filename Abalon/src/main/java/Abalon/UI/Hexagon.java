6d8a76adfd8225ed1e6134a4570ce2eec5f5ecef
Aaron Schapira <aaronschapira@gmail.com>
2020-11-24 10:08:11 +0100

    Starting of putting the AI in the UI

--- Abalon/src/main/java/Abalon/UI/Hexagon.java
+++ Abalon/src/main/java/Abalon/UI/Hexagon.java
@@ -25,12 +25,6 @@ import javafx.event.EventHandler;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.KeyCode;  
 
-/**
- * TODO Add marbles to the board and create Home page
- * Home Page: Aaron
- * Marbles: Aaron + Adèle 
- */
-
 public class Hexagon extends Application {
 
     private final double WIDTH = 1270;
@@ -118,8 +112,33 @@ public class Hexagon extends Application {
 
             @Override
             public void run() {
-                Abalon game = new Abalon(board, p1, p2);
-                game.runGame();
+                if(HomePage.gameChoice.getValue().equals("Human VS Human")){
+                    System.out.println("Choice of game: Human vs Human");
+                    Abalon game = new Abalon(board, p1, p2);
+                    game.runGame();
+                }
+                else if (HomePage.gameChoice.getValue().equals("Alpha-Beta vs Human")){
+                    System.out.println("Choice of game: Alpha-Beta vs Human");
+                    //replace p2 by the AI
+                    Abalon game = new Abalon(board, p1, p2);
+                    game.runGame();
+                }
+                else if (HomePage.gameChoice.getValue().equals("MCTS vs Human")){
+                    System.out.println("Choice of game: MCTS vs Human");
+                    //replace p2 by the AI
+                    Abalon game = new Abalon(board, p1, p2);
+                    game.runGame();
+                }
+                else if (HomePage.gameChoice.getValue().equals("Alpha-Beta vs MCTS")){
+                    System.out.println("Choice of game: Alpha-Beta vs MCTS");
+                    //replace p1 and p2 by the bots
+                    Abalon game = new Abalon(board, p1, p2);
+                    game.runGame();
+                }
+               else{
+                    System.out.println("Please choose a valid game mode");
+                    //exit the game
+                }
             }
         });
         gameThread.setDaemon(false);
