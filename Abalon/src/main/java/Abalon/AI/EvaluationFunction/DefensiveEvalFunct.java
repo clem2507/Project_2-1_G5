ea6a13e1c21194b2de7df1108a1d1fa23465266f
clemdetry <c.detry@student.maastrichtuniversity.nl>
2021-01-14 11:10:25 +0100

    je push

--- Abalon/src/main/java/Abalon/AI/EvaluationFunction/DefensiveEvalFunct.java
+++ Abalon/src/main/java/Abalon/AI/EvaluationFunction/DefensiveEvalFunct.java
@@ -25,8 +25,8 @@ public class DefensiveEvalFunct extends EvaluationFunction{
 
      HEURISTICS:
 
-     v1 = the current player board center distance - opponent (difference)
-     v2 = the current player marbles cohesion - opponent (difference)
+     v1 = the current player board center distance
+     v2 = the current player marbles cohesion
      v4 = count of breaking opponent line patterns
      v6 = difference between root current player marble count and actual board count
 
@@ -45,10 +45,10 @@ public class DefensiveEvalFunct extends EvaluationFunction{
     private static int v4;
     private static int v6;
 
-    private static double w1 = -2;
-    private static double w2 = 2;
-    private static double w4 = 100;
-    private static double w6 = 70;
+    private static double w1 = -5;
+    private static double w2 = 5;
+    private static double w4 = 150;
+    private static double w6 = -10000;
 
 
     public DefensiveEvalFunct(int currentPlayer, int[][] cellColor, int[][] rootCellColor) {
@@ -59,15 +59,15 @@ public class DefensiveEvalFunct extends EvaluationFunction{
 
         //v1
         int ownMarblesCenterDistance;
-        int opponentMarblesCenterDistance;
+        //int opponentMarblesCenterDistance;
 
         // v2
         int ownMarblesNeighbour;
-        int opponentMarblesNeighbour;
+        //int opponentMarblesNeighbour;
 
         // v4
         int ownStrengthenGroupCount;
-        int opponentStrengthenGroupCount;
+        //int opponentStrengthenGroupCount;
 
         // v6
         int currentStateOwnMarblesCount = NeutralEvalFunct.countMarbles(currentPlayer, cellColor);
@@ -75,30 +75,31 @@ public class DefensiveEvalFunct extends EvaluationFunction{
 
         if (currentPlayer == 1) {
             ownMarblesCenterDistance = NeutralEvalFunct.centerDistance(currentPlayer, cellColor);
-            opponentMarblesCenterDistance = NeutralEvalFunct.centerDistance(currentPlayer+1, cellColor);
+            //opponentMarblesCenterDistance = NeutralEvalFunct.centerDistance(currentPlayer+1, cellColor);
 
             ownMarblesNeighbour = NeutralEvalFunct.marblesNeighbourhood(currentPlayer, cellColor);
-            opponentMarblesNeighbour = NeutralEvalFunct.marblesNeighbourhood(currentPlayer+1, cellColor);
+            //opponentMarblesNeighbour = NeutralEvalFunct.marblesNeighbourhood(currentPlayer+1, cellColor);
 
             ownStrengthenGroupCount = NeutralEvalFunct.strengthenGroupPattern(currentPlayer, cellColor);
-            opponentStrengthenGroupCount = NeutralEvalFunct.strengthenGroupPattern(currentPlayer+1, cellColor);
-
+            //opponentStrengthenGroupCount = NeutralEvalFunct.strengthenGroupPattern(currentPlayer+1, cellColor);
         }
         else {
             ownMarblesCenterDistance = NeutralEvalFunct.centerDistance(currentPlayer, cellColor);
-            opponentMarblesCenterDistance = NeutralEvalFunct.centerDistance(currentPlayer-1, cellColor);
+            //opponentMarblesCenterDistance = NeutralEvalFunct.centerDistance(currentPlayer-1, cellColor);
 
             ownMarblesNeighbour = NeutralEvalFunct.marblesNeighbourhood(currentPlayer, cellColor);
-            opponentMarblesNeighbour = NeutralEvalFunct.marblesNeighbourhood(currentPlayer-1, cellColor);
+            //opponentMarblesNeighbour = NeutralEvalFunct.marblesNeighbourhood(currentPlayer-1, cellColor);
 
             ownStrengthenGroupCount = NeutralEvalFunct.strengthenGroupPattern(currentPlayer, cellColor);
-            opponentStrengthenGroupCount = NeutralEvalFunct.strengthenGroupPattern(currentPlayer-1, cellColor);
-
+            //opponentStrengthenGroupCount = NeutralEvalFunct.strengthenGroupPattern(currentPlayer-1, cellColor);
         }
 
-        v1 = opponentMarblesCenterDistance - ownMarblesCenterDistance;
-        v2 = ownMarblesNeighbour - opponentMarblesNeighbour;
-        v4 = ownStrengthenGroupCount - opponentStrengthenGroupCount;
+        //v1 = opponentMarblesCenterDistance - ownMarblesCenterDistance;
+        v1 = ownMarblesCenterDistance;
+        //v2 = ownMarblesNeighbour - opponentMarblesNeighbour;
+        v2 = ownMarblesNeighbour;
+        //v4 = ownStrengthenGroupCount - opponentStrengthenGroupCount;
+        v4 = ownStrengthenGroupCount;
         v6 = rootOwnMarblesCount - currentStateOwnMarblesCount;
 
     }
@@ -106,16 +107,16 @@ public class DefensiveEvalFunct extends EvaluationFunction{
     @Override
     public double evaluate(){
 
-        int turn = Abalon.numberOfTurn;
+        //int turn = Abalon.numberOfTurn;
         double score = 0;
 
-        if(turn <= 4){
-            EvaluationFunction neut = new NeutralEvalFunct(currentPlayer, cellColor, rootCellColor);
-            score = neut.evaluate();
-        }else{
+        //if(turn <= 4){
+            //EvaluationFunction neut = new NeutralEvalFunct(currentPlayer, cellColor, rootCellColor);
+            //score = neut.evaluate();
+        //}else{
             computeValues();
-            score = (w1 * v1 + w2 * v2 + w4 * v4) - w6 * v6;
-        }
+            score = w1*v1 + w2*v2 + w4*v4 + w6*v6;
+        //}
 
         return score;
     }
